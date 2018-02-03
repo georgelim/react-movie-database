8d71a309c235d403645c4b33cacb6b33c5f1729e
gliberis <gliberis>
2018-02-03 14:22:26 +0000

    init commit

---
+++ public/data/jobs.js
@@ -0,0 +1,339 @@
+[
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjE=",
+            "createdDate": "2017-06-18T11:49:00.558456+00:00",
+            "duration": "",
+            "salary": "",
+            "description": "መስፍን ዋቆ ፊልም ፕሮዳክሽን በቅርቡ ለሚጀምረዉ አዲስ ተከታታይ የቴሌቪዥን ድራማ ከዚህ በታች የተገለፀዉን መስፈርት የሚያሟላ መሪ ወንድ ተዋናይ ይፈልጋል</br>\r\n<ul>\r\n<li>ቁመት፡- 1ሜከ70-1ሜከ75</li>\r\n<li> የቆዳ ቀለም፡- ጠይም </li>\r\n<li> ክብደት፡-ከ ቀጠን ያለ እስከ መካከለኛ  </li>\r\n<li> ብዛት ፡- 1  </li>\r\n</ul>\r\nተፈላጊ ችሎታ፡-የግድ በትወና ከዚህ በፊት የሰራ መሆን የለበትም ግን የትወና ስራዉን የሚሻማ ሌላ ስራ ያለዉ ሊሆን አይገባም\r\nማሳሰቢያ፡- ፡ለተከታታይ ድራማዉ የሚፈለገዉ ተዋናይ በድራማዉ ላይ መሪ ገፀባህሪ (major character) ስለሆነ አመልካቾች ሙሉ ግዜያቸዉን መስጠት የሚችሉና ሙያዉን የሚያከብሩ እንዲሆኑ ይጠበቃል፡፡ አመልካቾች ሙሉ ፊታቸዉን እና የተወሰነ ደረት እና ትከሻ የሚያሳይ  ፎቶ በሚከተሉት ስልክ ቁጥሮች በቫይበር ከሐምሌ22/2008-ሀምሌ24/2008 ብቻ ይላኩ፡፡</br>\r\n\r\n0910223424 </br>\r\n0910310978 </br>\r\nመልካም እድል !!! መስፍን ፊልም ፕሮዳክሽን",
+            "closingDate": "2016-09-30",
+            "productionDate": null,
+            "jobTitle": "Actor wanted ",
+            "slug": "actor-wanted",
+            "genderMf": "MALE",
+            "ageLevel": "A_5",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToy"
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTox"
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjEz",
+            "createdDate": "2017-12-12T05:02:13.843488+00:00",
+            "duration": "",
+            "salary": "",
+            "description": "<p>\r\n    በቴሌቪዥን ድራማ ላይ መተወን ለምትፈልጉ <a href=\"https://etmdb.com/en/company/96/tesfaye-film-production-promotion\">ከተስፋዬ ፊልም\r\n    ፕሮዳክሽንና ፕሮሞሽን</a> የተዋንያን ምልመላ ጥሪ<br>\r\n    <a href=\"https://etmdb.com/en/movie/500/lucy\">ሉሲ</a> የሚል ርዕስ በተሰጠውና ከፋና ቴሌቪዥን ጋር በመተባር ለማቅረብ በዝግጅት ላይ ባለው ተከታታይ\r\n    የቴሌቪዥን ድራማ ላይ መሳተፍ የምትፈልጉ ዕድሜያችሁ ከ16 ዓመት\r\n    እስከ 65 ዓመት የሆናችሁ ጀማሪና ነባር ተዋንያን: ከሰኞ ታኅሳስ 9/ 2010 እስከ ሰኞ ታህሳስ 16/2010 ድረስ ዕሁድን ጨምሮ ዘወትር በሥራ ሰዓት: ሺ ሰማንያ ከራስ አምባ ሆቴል\r\n    አጠገብ በሚገኘው ቢሯችን በመምጣት: ለምልመላና መረጣ\r\n    (Audition) በአካል በመቅረብ መመዝገብ ትችላላቻሁ፡፡\r\n</p>\r\n<p>ማስታወሻ\r\n<li>\r\n    በተለይ ዕድሜዋ ከ 30–40 የሆነ ቁመቷ ከ 1ሜትር ከ65 በላይ የሆነች የእንግሊዝኛ ቋንቋን በጥሩ ልሳን መናገር የምትችል የዮጋ ወይም የራስ መከላከል (Self-defense)\r\n    ችሎታና\r\n    የሰውነት ቅልጥፍና ያላትና\r\n</li>\r\n<li>\r\n    ዕድሜዋ ከ20 እእከ 25 የሆነ ቀልጣፋ ዘመናዊና የፈረንሳይኛ ቋንቋ የመናገር ችሎታ ያላት ብቁ ተዋናይት ቀዳሚ ተፈላጊ ናቸው፡፡\r\n</li>\r\nለበለጠ መረጃ: በስልክ ቁጥሮች <a href=\"tel:+251911484335\">+251 (0)911484335</a> or <a href=\"tel:+251988984406\">+251 (0)\r\n    988984406</a> መደወል ትችላላችሁ::\r\n</p>",
+            "closingDate": "2017-12-25",
+            "productionDate": "2018-01-01",
+            "jobTitle": "Call for audition-Lucy TV-series",
+            "slug": "call-for-audition-lucy-tv-series",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_8",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxOA=="
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxOQ=="
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjM=",
+            "createdDate": "2017-06-18T11:49:57.338521+00:00",
+            "duration": "",
+            "salary": "",
+            "description": "        CinemaHall is looking for partners in Ethiopia.\r\n        <a href=\"https://www.facebook.com/cinemahall.org/\"> CinemaHall </a>International Organization together with\r\n        Kinolife Distribution is looking for talented and active people (as well as companies) who love cinema and\r\n        have\r\n        desire to represent a new extremely interesting global film distribution project.\r\n        For more details please apply with <a href=\"mailto:info@cinemahall.org?Subject=Partnership%20Ethiopia\"\r\n                                              target=\"_top\">info@cinemahall.org</a> or send a private message to\r\n        <a href=\"https://www.facebook.com/isvidler\"> Illia Svidler. </a>\r\n        CinemaHall - is the International Film Organization, full member of the European film association of the\r\n        organizations of young professionals, students and film enthusiasts NISI MASA, member of International\r\n        Federation of Film Societies.<i class=\"fa fa-external-link-square\"></i> <a href=\"http://cinemahall.org/\">\r\n        CinemaHall</a> |\r\n        <i class=\"fa fa-facebook\"></i> <a\r\n            href=\"https://www.facebook.com/cinemahall.org/\"> CinemaHall </a> |\r\n        <i class=\"fa fa-envelope\"></i><a\r\n            href=\"mailto:info@cinemahall.org?Subject=Partnership%20Ethiopia\" target=\"_top\">\r\n        info@cinemahall.org</a> <br/>\r\n\r\n        Kinolife is a movie distribution company, situated in the heart of Europe.\r\n        <i class=\"fa fa-external-link-square\"></i> <a\r\n            href=\"http://distribution.kinolife.eu\"> Kinolife Distribution </a> |\r\n        <i class=\"fa fa-facebook\"></i> <a\r\n            href=\"https://www.facebook.com/distribution.kinolife.eu\"> Kinolife </a> |\r\n        <i class=\"fa fa-envelope\"></i> <a\r\n            href=\"mailto:info@cinemahall.org?Subject=Partnership%20Ethiopia\" target=\"_top\"> info@kinolife.eu</a>",
+            "closingDate": "2016-09-30",
+            "productionDate": null,
+            "jobTitle": "CinemaHall is looking for partners in Ethiopia",
+            "slug": "cinemahall-is-looking-for-partners-in-ethiopia",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_1",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTo3"
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjU=",
+            "createdDate": "2017-06-18T11:47:24.422338+00:00",
+            "duration": "",
+            "salary": "",
+            "description": " አቢሲንያ ፊልም ፕሮዳክሽን ተዋናይ ይፈልጋሉ በማንኛውም እድሜ ላይ ያላቹ መመዝገብ ትችላላቹ::\r\n<br/> ለበለጠ መረጃ   0912 67 45 13  \r\n<br> የቢሮ 011 8 52 75 75  መመዝገብ ትችላላቹ ::\r\n<br/> አቢሲንያ ፕሮዳክሽን ኑርልኝን ምርጥ  ስራ ለህዝብ አቅርቧል::",
+            "closingDate": "2017-01-31",
+            "productionDate": null,
+            "jobTitle": " Actors/Actress wanted",
+            "slug": "actorsactress-wanted",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_1",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxMw=="
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxMg=="
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjY=",
+            "createdDate": "2017-06-18T11:50:01.481328+00:00",
+            "duration": "",
+            "salary": "",
+            "description": "ፊልም ላይ የመተወን ፍላጎት ያላቹ አለም ዘጠኝ ሲትኮም ድራማ ላይ ተወዳድራቹ መስራት ስለምችሉ፣ 0977423119 ላይ በመደወል ተመዝገቡ። <br>\r\nWe are looking for different talents for the best comedy sitcom drama, 'Alem Zetegn'. For the detailes and auditioning, please contact us through our phone +251977423119",
+            "closingDate": "2017-07-31",
+            "productionDate": "2017-08-01",
+            "jobTitle": "Talent Wanted For 'Alem Zetegn' Sitcom Drama",
+            "slug": "talent-wanted-for-alem-zetegn-sitcom-drama",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_1",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxNA=="
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxNQ=="
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjEy",
+            "createdDate": "2017-09-25T06:46:58.359252+00:00",
+            "duration": "",
+            "salary": "Negotiable",
+            "description": "We are seeking some experienced sitcom drama writers to join our team. The applicant must have reference or previous work ready to show and have high communication skills. We also require that the applicant is highly motivated to work in pressure, participate & collaborate with the team.  Payment is negotiable and it is done per episode. \r\n</br>\r\nPlease send your phone and contact details for \r\n<a href=\"mailto: semagngeta@gmail.com?subject=Sitcom%20writer\"> semagngeta@gmail.com</a>",
+            "closingDate": "2017-11-30",
+            "productionDate": "2017-10-01",
+            "jobTitle": "Sitcom writers needed",
+            "slug": "sitcom-writers-needed",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_7",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxNw=="
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxNg=="
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjI=",
+            "createdDate": "2017-06-18T11:48:47.877790+00:00",
+            "duration": "",
+            "salary": "",
+            "description": "<a href=\"https://www.facebook.com/Taharqa-entertainmentቲሃርቃ-ኢንተርቴመንት-489017421292376\">ቲሃርቃ\r\n    አኢንተርቴመንት </a> ባዘጋጀው በሃገራችን በአይነቱ ልዩ የሆነ እና የመጀመሪያው የአጭር ፊልም ውድድር በቴሌቪዥን ሾው ተዘጋጅቷል። እርሶም በዚህ ውድድር ላይ እንዲሳተፉ ተጋብዘዋል።\r\nተወዳዳሪዎች <br/>\r\n<ol>\r\n    <li>በማንኛውም እድሜ ክልል ላይ ያሉ መወዳደር ይችላሉ</li>\r\n    <li>በቡድን ወይንም በግል መወዳደር ይችላሉ</li>\r\n    <li> ከሶስት ደቂቃ ያልበለጠ ስራ ያቀርባሉ</li>\r\n    <li> በኢትዮጵያ ብሄር በሄረሰቦች ታሪክ ትውፊት እና ባህል ወግ ላይ ብቻ ትኩረት ያደረገ መሆን አለበት</li>\r\n</ol>\r\nመመዝገቢያ 150 ብር ብቻ <br/>\r\nለበለጠ መረጃ <br/>\r\n<ul>\r\n    <li><i class=\"fa fa-phone\"></i>  0913 663812 | 0913 374774</li>\r\n    <li> <i class=\"fa fa-phone\"></i>  0929 177364 | 0910 630443</li>\r\n    <li> <i class=\"fa fa-phone\"></i>  0910 032732 | 0910 922420</li>\r\n</ul>\r\nአድራሻ :- ልደታ ባልቻ ሆስፒታል ጀርባ፤ ኤሌክትሪክ ወርልድ ህንፃ  5 ፎቅ ቢሮ ቁጥር 405\r\n",
+            "closingDate": "2016-09-30",
+            "productionDate": null,
+            "jobTitle": "Short film ",
+            "slug": "short-film",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_1",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTo0"
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToz"
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTo1"
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTo2"
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjQ=",
+            "createdDate": "2017-06-18T11:48:31.958342+00:00",
+            "duration": "",
+            "salary": "",
+            "description": "ነጋሪት ፊልም አካዳሚ እና ፕሮዳክሽንስ ከጀርመኑ RUB ፊልም ካምፓኒ ጋር በመተባበር ለሚሰራው <b>\"Running Against the Wind\" </b> ለተባለው ኢንተርናሽናል ፊቸር ፊልም በርካታ ተዋንያን መርጦ እና አሰልጥኖ ለማሰራት ይፈልጋል።  <br/>\r\nየትወና ፍላጎት ያላቸው በሙሉ፥ ንጹህ ፎቶ ከመሰለታዊ መረጃዎች ጋር እንዲልኩልን በአክብሮት እንጠይቃለን። <br/>\r\n\r\nዕድሜ፡ <br/>\r\nለወንድ ከ8-60 ዓመት<br/>\r\nለሴት  ከ4-50 ዓመት<br/>\r\n\r\nተጨማሪ  መረጃ<br/>\r\n፦ ስም <br/>\r\n፦ ዕድሜ <br/>\r\n፦ ቁመት <br/>\r\n፦ ክብደት<br/>\r\n፦ ስልክ ቁጥር <br/>\r\n\r\n<u> የሚላክ የፎቶ ዓይነት </u> <br/>\r\nሙሉ ቁመት የሚያሳይ አንድ ፎቶ <br/>\r\nየመታወቂያ ዓይነት አንድ ጉርድ ፎቶ<br/>\r\nበጎን በኩል የሚያሳይ አንድ ጉርድ ፎቶ<br/>\r\n\r\n<u>ማሳሰቢያ </u>  <br/>\r\nፎቶዎቹ በፎቶሾፕ የተነካኩ ከሆኑ አንቀበልም።<br/>\r\nበስማርት ፎን የተነሳ በግልፅ  የሚታይ ንፁህ ፎቶ እንቀበላለን።<br/>\r\n \r\nአድራሻችን፥ <br/>\r\nነጋሪት ፊልም አካዳሚ እና ፕሮዳክሽንስ የፌስብክ ገፅ <a href=\"https://www.facebook.com/magicbulletfilmacademy/\">Facebook message</a>\r\n ላይ መረጃችሁን በInbox አስገቡልን።\r\nለአጭር ስልክ መረጃ በ+251 91 192 8330 ላይ ይደውሉ።\r\n\r\n",
+            "closingDate": "2016-10-31",
+            "productionDate": null,
+            "jobTitle": "Looking for multiple actress & actors ",
+            "slug": "looking-for-multiple-actress-actors",
+            "genderMf": "NOTSPECIFIED",
+            "ageLevel": "A_1",
+            "jobtagSet": {
+                "edges": [
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTo5"
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZTo4"
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxMA=="
+                        }
+                    },
+                    {
+                        "node": {
+                            "id": "Sm9iVGFnTm9kZToxMQ=="
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjEx",
+            "createdDate": "2017-06-18T16:31:49.395390+00:00",
+            "duration": "",
+            "salary": "522",
+            "description": "job_title",
+            "closingDate": "2017-06-09",
+            "productionDate": "2017-05-30",
+            "jobTitle": "Test-1",
+            "slug": "test-1",
+            "genderMf": "FEMALE",
+            "ageLevel": "A_5",
+            "jobtagSet": {
+                "edges": []
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjc=",
+            "createdDate": "2017-06-18T16:26:24.408496+00:00",
+            "duration": "",
+            "salary": "100",
+            "description": "I looked the documentation and several questions in SO, with no success. So, if someone has a way to achieve this, I would be extremely grateful.\r\n\r\n",
+            "closingDate": "2017-06-09",
+            "productionDate": "2017-06-08",
+            "jobTitle": "Test-5",
+            "slug": "test-5",
+            "genderMf": "FEMALE",
+            "ageLevel": "A_4",
+            "jobtagSet": {
+                "edges": []
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjg=",
+            "createdDate": "2017-06-18T16:27:28.695704+00:00",
+            "duration": "",
+            "salary": "100",
+            "description": "I looked the documentation and several questions in SO, with no success. So, if someone has a way to achieve this, I would be extremely grateful.\r\n\r\n",
+            "closingDate": "2017-06-09",
+            "productionDate": "2017-06-08",
+            "jobTitle": "Test-4",
+            "slug": "test-4",
+            "genderMf": "FEMALE",
+            "ageLevel": "A_4",
+            "jobtagSet": {
+                "edges": []
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjk=",
+            "createdDate": "2017-06-18T16:27:50.380888+00:00",
+            "duration": "",
+            "salary": "10005",
+            "description": "I looked the documentation and several questions in SO, with no success. So, if someone has a way to achieve this, I would be extremely grateful.\r\n\r\n",
+            "closingDate": "2017-06-09",
+            "productionDate": "2017-06-08",
+            "jobTitle": "Test-3",
+            "slug": "test-3",
+            "genderMf": "FEMALE",
+            "ageLevel": "A_4",
+            "jobtagSet": {
+                "edges": []
+            }
+        }
+    },
+    {
+        "node": {
+            "id": "Sm9iQm9hcmROb2RlOjEw",
+            "createdDate": "2017-06-18T16:29:21.882829+00:00",
+            "duration": "",
+            "salary": null,
+            "description": "I looked the documentation and several questions in SO, with no success. So, if someone has a way to achieve this, I would be extremely grateful.\r\n\r\n",
+            "closingDate": "2017-06-09",
+            "productionDate": "2017-06-08",
+            "jobTitle": "Test-2",
+            "slug": "test-2",
+            "genderMf": "FEMALE",
+            "ageLevel": "A_4",
+            "jobtagSet": {
+                "edges": []
+            }
+        }
+    }
+]
---
+++ src/components/Root.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { HashRouter as Router } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import { Route, Switch } from 'react-router-dom'
+
+import App from './App'
+import theme from '../config/Theme'
+
+const muiTheme = getMuiTheme(theme)
+
+const Root = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Router>
+      <Switch>
+        <Route path="/" component= {App} />
+      </Switch>
+    </Router>
+  </MuiThemeProvider>
+)
+
+export default Root
